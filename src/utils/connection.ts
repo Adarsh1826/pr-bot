96536940dfbd7dc109bde754cc91e8e10283f310
adarsh singh <adsredmi182615@gmail.com>
2026-01-07 11:07:35 +0530

    build fixed

--- src/utils/connection.ts
+++ src/utils/connection.ts
@@ -4,7 +4,7 @@ if (!process.env.REDIS_URL) {
   throw new Error("REDIS_URL is missing");
 }
 
-export const connection = new Redis(process.env.REDIS_URL, {
-  maxRetriesPerRequest: null,
-  tls: {},
-});
+export const connection = {
+  url: process.env.REDIS_URL!,
+  maxRetriesPerRequest: null, 
+};
